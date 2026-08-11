4296b3273943684b2f120c8181d07e754cfe3ee6
swmidddo <swmidddo@gmail.com>
2026-08-11 11:40:17 +1000

    Build v100760 - Dual-Mode URL Auth Restoration & Form Submit PreventDefault

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker auto-clearing and unregister script
-const CACHE_NAME = 'fly-fishing-v100750';
+const CACHE_NAME = 'fly-fishing-v100760';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
