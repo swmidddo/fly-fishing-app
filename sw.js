0dc3839848e8c9f79e9dfb7bc78f00a6761ba1e2
swmidddo <swmidddo@gmail.com>
2026-08-11 11:26:35 +1000

    Build v100730 - Fix Global window.closeAuthModal Declaration in Head Script

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker auto-clearing and unregister script
-const CACHE_NAME = 'fly-fishing-v100720';
+const CACHE_NAME = 'fly-fishing-v100730';
 
 self.addEventListener('install', (event) => {
     self.skipWaiting();
