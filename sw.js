160fb60a69243e6ae2731848e34a7601acedb213
swmidddo <swmidddo@gmail.com>
2026-08-24 10:02:49 +1000

    Release v101120: Complete Queensland Tidal and Freshwater Fisheries Regulations and Species Catalog

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Middo's Fly Fishing Backcountry Offline Service Worker
-const CACHE_NAME = 'fly-fishing-v101110';
+const CACHE_NAME = 'fly-fishing-v101120';
 
 // Core Local Assets to Pre-Cache on Install
 const CORE_ASSETS = [
